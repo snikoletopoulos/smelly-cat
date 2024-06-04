96793b999d7ba3f1e9bb0335459c116eda74b353
Stavros <snikoletopoulos@hotmail.com>
2024-06-04 18:51:13 +0300

    chore: remove base template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'smelly-cat';
-}
+export class AppComponent {}
