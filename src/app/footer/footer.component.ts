571dcd5bfa1b44fd75e5b3e0b261e20c43d330d5
Stavros <snikoletopoulos@hotmail.com>
2024-06-05 21:12:03 +0300

    feat(routing): make nav items navigatable

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,52 +1,50 @@
 import { Component } from "@angular/core";
-import { FooterNavLinkComponent } from "../footer-nav-link/footer-nav-link.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: "app-footer",
-  standalone: true,
-  imports: [FooterNavLinkComponent],
-  template: `
-    <footer class="bg-black py-6 space-y-10">
-      <section class="container mt-8 flex justify-between mx-auto text-white">
-        <div>LOGO</div>
+	selector: "app-footer",
+	standalone: true,
+	imports: [RouterModule],
+	template: `
+		<footer class="bg-black py-6 space-y-10">
+			<section class="container mt-8 flex justify-between mx-auto text-white">
+				<div>LOGO</div>
 
-        <nav
-          class="flex-1 gap-12 flex justify-center items-center font-extralight"
-        >
-          @for (link of links; track link.label) {
-            <app-footer-nav-link [href]="link.href">{{
-              link.label
-            }}</app-footer-nav-link>
-          }
-        </nav>
+				<nav
+					class="flex-1 gap-12 flex justify-center items-center font-extralight"
+				>
+					@for (link of links; track link.label) {
+						<a [routerLink]="link.href">{{ link.label }}</a>
+					}
+				</nav>
 
-        <div>SOCIAL LINKS</div>
-      </section>
+				<div>SOCIAL LINKS</div>
+			</section>
 
-      <p class="text-gray-200 text-center text-xs">
-        Copyright &#169; SmellyCat {{ currentYear }}
-      </p>
-    </footer>
-  `,
+			<p class="text-gray-200 text-center text-xs">
+				Copyright &#169; SmellyCat {{ currentYear }}
+			</p>
+		</footer>
+	`,
 })
 export class FooterComponent {
-  currentYear = new Date().getFullYear();
-  links: { href: string; label: string }[] = [
-    {
-      href: "/privacy-policy",
-      label: "Privacy Policy",
-    },
-    {
-      href: "/terms-conditions",
-      label: "Terms & Conditions",
-    },
-    {
-      href: "/blog",
-      label: "Blog",
-    },
-    {
-      href: "/support",
-      label: "Support",
-    },
-  ];
+	currentYear = new Date().getFullYear();
+	links: { href: string; label: string }[] = [
+		{
+			href: "/privacy-policy",
+			label: "Privacy Policy",
+		},
+		{
+			href: "/terms-conditions",
+			label: "Terms & Conditions",
+		},
+		{
+			href: "/blog",
+			label: "Blog",
+		},
+		{
+			href: "/support",
+			label: "Support",
+		},
+	];
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,44 +1,45 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: "app-header",
-  standalone: true,
-  imports: [],
-  template: `
-    <header class="border-b py-2 border-b-gray-400">
-      <section class="container flex justify-between mx-auto">
-        LOGO
+	selector: "app-header",
+	standalone: true,
+	imports: [RouterModule],
+	template: `
+		<header class="border-b py-2 border-b-gray-400">
+			<section class="container flex justify-between mx-auto">
+				<a routerLink="/">SMELLY CAT</a>
 
-        <nav class="flex justify-between gap-12 text-bold">
-          @for (link of links; track link.label) {
-            <p>{{ link.label }}</p>
-          }
-        </nav>
-      </section>
-    </header>
-  `,
+				<nav class="flex justify-between gap-12 text-bold">
+					@for (link of links; track link.label) {
+						<a [routerLink]="link.href">{{ link.label }}</a>
+					}
+				</nav>
+			</section>
+		</header>
+	`,
 })
 export class HeaderComponent {
-  links: { href: string; label: string }[] = [
-    {
-      href: "/adopt-kittiens",
-      label: "Adopt Kittens",
-    },
-    {
-      href: "/stories",
-      label: "Kitty Stories",
-    },
-    {
-      href: "/about-us",
-      label: "About us",
-    },
-    {
-      href: "/blog",
-      label: "Blog",
-    },
-    {
-      href: "/contact",
-      label: "Contact",
-    },
-  ];
+	links: { href: string; label: string }[] = [
+		{
+			href: "/adopt-kittens",
+			label: "Adopt Kittens",
+		},
+		{
+			href: "/stories",
+			label: "Kitty Stories",
+		},
+		{
+			href: "/about-us",
+			label: "About us",
+		},
+		{
+			href: "/blog",
+			label: "Blog",
+		},
+		{
+			href: "/contact",
+			label: "Contact",
+		},
+	];
 }
