33347a053d33f576714bb131fe42843125b978b4
Stavros <snikoletopoulos@hotmail.com>
2024-06-05 21:09:26 +0300

    feat(route): create catch-all route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { NotImplementedComponent } from "./not-implemented/not-implemented.component";
 
 export const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
     title: "Smelly Cat",
+  },
+
+  {
+    path: "**",
+    component: NotImplementedComponent,
+    title: "Not Implemented",
   },
 ];
