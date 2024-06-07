1e17dd753188c3e10103d0516b1d3722028c5bd3
Stavros <snikoletopoulos@hotmail.com>
2024-06-08 02:43:12 +0300

    style(config): add font family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{html,ts}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Manrope", "sans-serif"],
+			},
+			colors: {
+				primary: "#EF7878",
+				paper: "#EFF1F4",
+				muted: "#E1E4EA",
+				separator: "#D8D8D8",
+			},
+		},
+	},
+	plugins: [],
 };
