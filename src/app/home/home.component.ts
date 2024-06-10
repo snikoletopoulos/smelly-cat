6536d4fa98a2a4dd94dd0d719c7237f2a4ad80d9
Stavros <snikoletopoulos@hotmail.com>
2024-06-10 09:57:13 +0300

    fix: build

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
 import { ContactFormComponent } from "../contact-form/contact-form.component";
-import { LocationComponent } from "../location/location.component";
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [ContactFormComponent, LocationComponent],
+	imports: [ContactFormComponent],
 	template: `
 		<section
 			class="flex flex-col justify-between gap-14 pb-20 pt-10 md:flex-row md:pb-24 md:pt-14 lg:pb-[11.687em] lg:pt-20"
@@ -14,9 +13,7 @@ import { LocationComponent } from "../location/location.component";
 				<app-contact-form />
 			</div>
 
-			<div class="flex-1 text-center md:mt-24 md:text-right">
-				<app-location />
-			</div>
+			<div class="flex-1 text-center md:mt-24 md:text-right"></div>
 		</section>
 	`,
 })
